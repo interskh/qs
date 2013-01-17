483780accf9bdc42ca744e850d13fbacb9b18a86
Kyle Sun <kyle@bigkyle.com>
2013-01-17 00:01:41 -0800

    move javascript into a separate file

---
+++ app/assets/javascripts/foursquare.js
@@ -0,0 +1,82 @@
+var chart, chart1;
+$(document).ready(function() {
+  drawFirstLevel();
+  $('#back a').click(function(){
+    chart1.destroy();
+    drawFirstLevel();                
+  });
+});
+
+function drawFirstLevel() {
+  chart = new Highcharts.Chart( {
+    chart: {
+      renderTo: "foursquare_pie",
+      plotBackgroundColor: null,
+      plotBorderWidth: null,
+      plotShadow: true
+    },
+    title: {
+      text: "Checkins by Category"
+    },
+    plotOptions: {
+      pie: {
+        allowPointSelect: true,
+        cursor: 'pointer',
+        dataLabels: {
+            enabled: true,
+            color: '#000000',
+            connectorColor: '#000000',
+            formatter: function() {
+                return '<b>'+ this.point.name +'</b>'+ this.percentage.toFixed(1) +' %';
+            }
+        },
+        point: {
+          events: {
+            click: function() {
+              drawSecondLevel(this.name);
+            }
+          }
+        }
+      }
+    },
+    series: [{
+      type: 'pie',
+      name: 'Browser share',
+      data: data['all']
+    }]
+  });
+}
+
+function drawSecondLevel(name) {
+  chart.destroy();
+  chart1 = new Highcharts.Chart( {
+    chart: {
+      renderTo: "foursquare_pie",
+      plotBackgroundColor: null,
+      plotBorderWidth: null,
+      plotShadow: true
+    },
+    title: {
+      text: "Checkins by Category " + name
+    },
+    plotOptions: {
+      pie: {
+        allowPointSelect: true,
+        cursor: 'pointer',
+        dataLabels: {
+            enabled: true,
+            color: '#000000',
+            connectorColor: '#000000',
+            formatter: function() {
+                return '<b>'+ this.point.name +'</b>'+ this.percentage.toFixed(1) +' %';
+            }
+        }
+      }
+    },
+    series: [{
+      type: 'pie',
+      name: 'Browser share',
+      data: data['detailed'][name]
+    }]
+  });
+}
